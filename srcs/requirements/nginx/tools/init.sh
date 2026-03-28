82ff824b7dd40cdbd650a70c5939e3667edee217
brivera_vm <brivera@student.42madrid.com>
2026-03-28 19:56:42 +0100

    ngnix usas el .env

--- srcs/requirements/nginx/tools/init.sh
+++ srcs/requirements/nginx/tools/init.sh
@@ -6,5 +6,11 @@ mkdir -p /etc/nginx/ssl
 cp /run/secrets/server.crt /etc/nginx/ssl/server.crt
 cp /run/secrets/server.key /etc/nginx/ssl/server.key
 
+
+# Procesa la plantilla nginx.conf.template con las variables de entorno
+envsubst '${HTTPS_PORT} ${DOMAIN_NAME}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+#envsubst < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+
+
 exec nginx -g "daemon off;"
 
